8b3a6415325d2c820b5d2fe326edb65cb18012a0
Robin_tty <muellerobin95@gmail.com>
2018-04-06 15:10:39 +0200

    Add basic functionality to ActiveAircrafts class

--- src/acamo/ActiveAircrafts.java
+++ src/acamo/ActiveAircrafts.java
@@ -1,31 +1,37 @@
 package acamo;
 
-import java.util.HashMap;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.ArrayList;
+import java.util.*;
+
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
 import messer.*;
 
 //TODO: create hash map and complete all operations
 public class ActiveAircrafts implements Observer
 {
-	private HashMap<String , ArrayList<BasicAircraft>> activeAircrafts;    	// store the basic aircraft and use its Icao as key
-                                                                            // replace K and V with the correct class names
+	private HashMap<String , BasicAircraft> activeAircrafts;    	// store the basic aircraft and use its Icao as key
+																	// replace K and V with the correct class names
 
 	public ActiveAircrafts () {
 	}
 
-	public synchronized void store(String icao, BasicAircraft ac) {
+	public synchronized void store(String icao, BasicAircraft ac)
+    {
+		activeAircrafts.put(icao,ac);
 	}
 
-	public synchronized void clear() {
+	public synchronized void clear()
+    {
+	    activeAircrafts.clear();
 	}
 
-	public synchronized BasicAircraft retrieve(String icao) {
-		return ac;
+	public synchronized BasicAircraft retrieve(String icao)
+    {
+		return activeAircrafts.get(icao);
 	}
 
-	public synchronized ArrayList<BasicAircraft> values () {
+	public synchronized ArrayList<BasicAircraft> values ()
+    {
+        return new ArrayList<>(activeAircrafts.values());
 	}
 
 	public String toString () {
@@ -34,6 +40,8 @@ public class ActiveAircrafts implements Observer
 
 	@Override
 	// TODO: store arg in hashmap using the method above
-	public void update(Observable o, Object arg) {
+	public void update(Observable o, Object arg)
+    {
+	    this.store();
 	}
 }
