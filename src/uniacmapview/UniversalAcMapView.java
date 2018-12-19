3fd75a01806211443b84ef530a89edd30067eb36
Robin_tty <muellerobin95@gmail.com>
2018-12-19 04:43:08 +0100

    Add method do add planes to db

--- src/uniacmapview/UniversalAcMapView.java
+++ src/uniacmapview/UniversalAcMapView.java
@@ -1,7 +1,9 @@
 package uniacmapview;
+
 import java.util.*;
 import java.sql.*;
 import java.util.concurrent.CompletableFuture;
+
 import acmapview.ActiveAircrafts;
 import de.saring.leafletmap.*;
 import javafx.application.Application;
@@ -26,8 +28,7 @@ import messer.BasicAircraft;
 import messer.Messer;
 import senser.Senser;
 
-public class UniversalAcMapView extends Application implements Observer
-{
+public class UniversalAcMapView extends Application implements Observer {
     private final ActiveAircrafts activeAircrafts = new ActiveAircrafts();
     private final TableView<BasicAircraft> table = new TableView<>();
     private final ObservableList<BasicAircraft> aircraftList = FXCollections.observableArrayList();
@@ -35,7 +36,7 @@ public class UniversalAcMapView extends Application implements Observer
 
     private final double latitude = 48.7433425;
     private final double longitude = 9.3201122;
-    private final int distance = 150;
+    private final int distance = 50;
     private final boolean haveConnection = true;
     private int selectedIndex;
 
@@ -48,24 +49,23 @@ public class UniversalAcMapView extends Application implements Observer
     private CompletableFuture<Worker.State> loadState;
     private LeafletMapView lm;
     private final Marker homeMarker = new Marker(new LatLong(latitude, longitude), "home", "home", 0);
-    
+
     public static void main(String[] args) {
-		launch(args);
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage)
-    {
+    public void start(Stage stage) {
         String urlString = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json";
         PlaneDataServer server;
-        if(haveConnection)
+        if (haveConnection)
             server = new PlaneDataServer(urlString, latitude, longitude, distance);
         else
             server = new PlaneDataServer();
 
         ////////// connect to database //////////
         // Create a variable for the connection string
-        try{
+        try {
             // Establish connection
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String connectionUrl = "jdbc:sqlserver://134.108.190.89:1433;" + "databaseName=WKB4_DB2_Projekt;user=wkb4;password=wkb4";
@@ -77,37 +77,19 @@ public class UniversalAcMapView extends Application implements Observer
             rs = stmt.executeQuery(SQL);
 
             // Iterate trough the data in the result set and display it
-            while(rs.next()){
-                System.out.println("Id: " + rs.getString(1) + " - icao: " + rs.getString(2) + " - operator: " + rs.getString(3) + " - species: " + rs.getString(4) + " - posTime: " + rs.getString(5) + " - latitude: " + rs.getString(6) + " - longitude: " + rs.getString(7) +" - speed: " + rs.getString(8) + " - trak: " + rs.getString(9) + " - altitude: " + rs.getString(10));
+            while (rs.next()) {
+                System.out.println("Id: " + rs.getString(1) + " - icao: " + rs.getString(2) + " - operator: " + rs.getString(3) + " - species: " + rs.getString(4) + " - posTime: " + rs.getString(5) + " - latitude: " + rs.getString(6) + " - longitude: " + rs.getString(7) + " - speed: " + rs.getString(8) + " - trak: " + rs.getString(9) + " - altitude: " + rs.getString(10));
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if (rs != null) try {
-                rs.close();
-            } catch (SQLException e) {
-                System.out.println("Exception while closing rs");
-                e.printStackTrace();
-            }
-            if (stmt != null) try {
-                stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Exception while closing stmt");
-            }
-            if (con != null) try {
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Exception while closing con");
-            }
+            CloseResultSet();
+            CloseStatement();
             System.out.println("Connection to database successfully initiated!");
         }
-
-
-            ////////// connect to database //////////
+        ////////// connect to database //////////
 
         // start modules
         new Thread(server).start();                           //Server start
@@ -120,7 +102,8 @@ public class UniversalAcMapView extends Application implements Observer
 
         //activeAircrafts and Acamo needs to observe messer
         messer.addObserver(activeAircrafts);
-        messer.addObserver(this);
+        new Thread(activeAircrafts).start();
+        activeAircrafts.addObserver(this);
 
         //fill table column headers
         ArrayList<String> attributeNames = BasicAircraft.getAttributesNames();
@@ -142,7 +125,7 @@ public class UniversalAcMapView extends Application implements Observer
         root.getChildren().add(table);
 
         //Scene
-        Scene scene = new Scene(root,1350,900, Color.CYAN);
+        Scene scene = new Scene(root, 1350, 900, Color.CYAN);
         stage.setScene(scene);
         stage.setTitle("Acamo");
         stage.sizeToScene();
@@ -166,54 +149,62 @@ public class UniversalAcMapView extends Application implements Observer
         vB.getChildren().add(menuBar);
 
         //exit handler
-        exit.setOnAction(event -> System.exit(0));
+        exit.setOnAction(event -> {
+            System.exit(0);
+            if (con != null) try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Exception while closing con");
+            }
+        });
 
         //reset location handler
         resLoc.setOnAction(event -> {
-                    //Dialog for reset location handler
-                    Dialog<String[]> resLocDialog = new Dialog<>();
-                    resLocDialog.setHeaderText("Please provide the new Coordinates");
-                    //resLoc.setGraphic();
-                    resLocDialog.getDialogPane().getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
-                    GridPane grid = new GridPane();         //content
-                    grid.setHgap(10);
-                    grid.setVgap(10);
-                    grid.setPadding(new Insets(20, 150, 10, 10));
-                    //fields
-                    TextField latIn = new TextField();
-                    latIn.setPromptText("latitude");
-                    TextField longIn = new TextField();
-                    longIn.setPromptText("longitude");
-                    //add fields
-                    grid.add(new Label("latitude:"), 0, 0);
-                    grid.add(latIn, 1, 0);
-                    grid.add(new Label("longitude:"), 0, 1);
-                    grid.add(longIn, 1, 1);
-                    // Enable/Disable apply button depending on whether a username was entered.
-                    Node applyButton = resLocDialog.getDialogPane().lookupButton(ButtonType.APPLY);
-                    applyButton.setDisable(true);
-                    //listen to input
-                    latIn.textProperty().addListener((observable, oldValue, newValue) -> applyButton.setDisable(newValue.trim().isEmpty()));
-                    //add content
-                    resLocDialog.getDialogPane().setContent(grid);
-                    //return values
-                    resLocDialog.setResultConverter(dialogButton -> {
-                        if (dialogButton == ButtonType.APPLY) {
-                            String retArr[] = new String[2];
-                            retArr[0] = latIn.getText();
-                            retArr[1] = longIn.getText();
-                            return retArr;
-                        }
-                        return null;
-                    });
-                    //collect result
-                    Optional<String[]> result = resLocDialog.showAndWait();
-                    try {
-                        result.ifPresent(strings -> resetLocation(Double.parseDouble(strings[0]), Double.parseDouble(strings[1]), distance, server));
-                    } catch(NumberFormatException e){
-                        System.out.println("Input wasn't in the right format");
-                    }
-                });
+            //Dialog for reset location handler
+            Dialog<String[]> resLocDialog = new Dialog<>();
+            resLocDialog.setHeaderText("Please provide the new Coordinates");
+            //resLoc.setGraphic();
+            resLocDialog.getDialogPane().getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
+            GridPane grid = new GridPane();         //content
+            grid.setHgap(10);
+            grid.setVgap(10);
+            grid.setPadding(new Insets(20, 150, 10, 10));
+            //fields
+            TextField latIn = new TextField();
+            latIn.setPromptText("latitude");
+            TextField longIn = new TextField();
+            longIn.setPromptText("longitude");
+            //add fields
+            grid.add(new Label("latitude:"), 0, 0);
+            grid.add(latIn, 1, 0);
+            grid.add(new Label("longitude:"), 0, 1);
+            grid.add(longIn, 1, 1);
+            // Enable/Disable apply button depending on whether a username was entered.
+            Node applyButton = resLocDialog.getDialogPane().lookupButton(ButtonType.APPLY);
+            applyButton.setDisable(true);
+            //listen to input
+            latIn.textProperty().addListener((observable, oldValue, newValue) -> applyButton.setDisable(newValue.trim().isEmpty()));
+            //add content
+            resLocDialog.getDialogPane().setContent(grid);
+            //return values
+            resLocDialog.setResultConverter(dialogButton -> {
+                if (dialogButton == ButtonType.APPLY) {
+                    String retArr[] = new String[2];
+                    retArr[0] = latIn.getText();
+                    retArr[1] = longIn.getText();
+                    return retArr;
+                }
+                return null;
+            });
+            //collect result
+            Optional<String[]> result = resLocDialog.showAndWait();
+            try {
+                result.ifPresent(strings -> resetLocation(Double.parseDouble(strings[0]), Double.parseDouble(strings[1]), distance, server));
+            } catch (NumberFormatException e) {
+                System.out.println("Input wasn't in the right format");
+            }
+        });
 
         //stop server handler
         stopServer.setOnAction(event -> server.stop());
@@ -228,12 +219,12 @@ public class UniversalAcMapView extends Application implements Observer
         //fill AircraftInfo
         Label header = new Label("Aircraft Information");                       //Header (Aircraft Information)
         header.setFont(gFont);
-        gPan.add(header,0,0,2,1);              //span header across the two columns of gPan
-        for(int i = 0; i < attributeNames.size(); i++)                              //fill attribute names
+        gPan.add(header, 0, 0, 2, 1);              //span header across the two columns of gPan
+        for (int i = 0; i < attributeNames.size(); i++)                              //fill attribute names
         {
             Text tmp = new Text(attributeNames.get(i) + ":");
             tmp.setFont(gFont);
-            gPan.add(tmp,0, i + 1);
+            gPan.add(tmp, 0, i + 1);
         }
         vB.getChildren().add(gPan);
 
@@ -255,8 +246,7 @@ public class UniversalAcMapView extends Application implements Observer
             lm.addMarker(homeMarker);                                                                                      //add to map and remember the returned String
 
             //create plane icons
-            for(int i = 0; i <= 24; i++)
-            {
+            for (int i = 0; i <= 24; i++) {
                 String number = String.format("%02d", i);
                 lm.addCustomMarker("plane" + number, "planeIcons/plane" + number + ".png");
             }
@@ -271,56 +261,117 @@ public class UniversalAcMapView extends Application implements Observer
                 uaValues = BasicAircraft.getAttributesValues(ua);                   //gather values trough BasicAircraft function
                 int valIndex = 1;
 
-                try{gPan.getChildren().remove(9,gPan.getChildren().size());}                              //indexes to be removed
-                catch(IndexOutOfBoundsException | IllegalArgumentException e){ e.printStackTrace(); }     //remove existing content
+                try {
+                    gPan.getChildren().remove(9, gPan.getChildren().size());
+                }                              //indexes to be removed
+                catch (IndexOutOfBoundsException | IllegalArgumentException e) {
+                    e.printStackTrace();
+                }     //remove existing content
                 for (Object o : uaValues)
                     try {
                         Text tmp = new Text(o.toString());
                         tmp.setFont(gFont);                                          //enlarge text, change font
-                        gPan.add(tmp,1, valIndex);                       //display values in column 1 of GridPane
+                        gPan.add(tmp, 1, valIndex);                       //display values in column 1 of GridPane
                         valIndex++;                                                  //index moves next element 1 row down
-                    } catch(java.lang.NullPointerException e) { System.out.println("gPan exception"); e.printStackTrace(); }
+                    } catch (java.lang.NullPointerException e) {
+                        System.out.println("gPan exception");
+                        e.printStackTrace();
+                    }
             }
         });
 
         //handle click on map
-        loadState.whenComplete((state, throwable) -> lm.onMapClick(latLong -> resetLocation(latLong.getLatitude(),latLong.getLongitude(), distance, server)));
+        loadState.whenComplete((state, throwable) -> lm.onMapClick(latLong -> resetLocation(latLong.getLatitude(), latLong.getLongitude(), distance, server)));
     }
 
-    private String acIconPicker(double track)
-    {
+    private void CloseStatement() {
+        if (stmt != null) try {
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Exception while closing stmt");
+        }
+    }
+
+    private void CloseResultSet() {
+        if (rs != null) try {
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Exception while closing rs");
+            e.printStackTrace();
+        }
+    }
+
+    private String acIconPicker(double track) {
         int iTrack = (int) Math.round(track / 15);                //360 Degrees, 24 PlaneIcons -> 360/24=15 - double value is required to round correctly
         return "plane" + String.format("%02d", iTrack);           //select right aircraft icon
     }
 
     private void resetLocation(double lat, double lng, int distance, PlaneDataServer server) {
         server.resetLocation(lat, lng, distance);           //reset server location
-        try { Thread.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         activeAircrafts.clear();
         aircraftList.clear();
         for (Marker anMarker : markerList.values()) {
             lm.removeMarker(anMarker);
         }
-        homeMarker.move(new LatLong(lat,lng));
+        homeMarker.move(new LatLong(lat, lng));
         //markerList.clear();                             //clear all markers
         lm.setView(new LatLong(lat, lng), 8);
     }
 
-    private void insertIntoDb(ArrayList<BasicAircraft> acList){
-        SQL = "SELECT icao FROM dbo.romuit02_Planes";
+    private void CheckDbForPlane(ArrayList<BasicAircraft> acList) {
+        ArrayList<String> results = new ArrayList<>();
         try {
+            // Check if plane already in database
+            SQL = "SELECT icao FROM dbo.romuit02_Planes";
             stmt = con.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
             rs = stmt.executeQuery(SQL);
+
+            while(rs.next()){
+                results.add(rs.getString(1));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            CloseResultSet();
+            CloseStatement();
+        }
+        for(BasicAircraft ac: acList){
+            if(results.contains(ac.getIcao())) continue;
+            AddToDb(ac);
         }
     }
 
-    private void clearMarkers(){
+    private int getMaxId(){
+        try {
+            SQL = "SELECT MAX(id) FROM dbo.romuit02_Planes";
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(SQL);
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    private void AddToDb(BasicAircraft ac) {
+        SQL = "INSERT INTO romuit02_Planes VALUES (" + (getMaxId()+ 1) + ",'" + ac.getIcao() + "','" + ac.getOperator() + "'," + ac.getSpecies() + ",'" + new Timestamp(ac.getPosTime().getTime()) + "'," + ac.getCoordinate().getLatitude() + "," + ac.getCoordinate().getLongitude() + "," + ac.getSpeed() + "," + ac.getTrak() + "," + ac.getAltitude() + ")";
+        try {
+            stmt = con.createStatement();
+            stmt.execute(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(SQL.toString());
+    }
+
+    private void clearMarkers() {
         for (Marker anMarker : markerList.values()) {
             lm.removeMarker(anMarker);
         }
@@ -339,9 +390,8 @@ public class UniversalAcMapView extends Application implements Observer
         aircraftList.clear();                                //clear observable list
         aircraftList.addAll(activeAircrafts.values());       //add all from ActiveAircrafts Object
 
-
         ////////// insert planes into database //////////
-        //insertIntoDb(activeAircrafts.values());
+        CheckDbForPlane(activeAircrafts.values());
         ////////// insert planes into database //////////
 
         //update acIcons on map
@@ -364,7 +414,8 @@ public class UniversalAcMapView extends Application implements Observer
                         });
                     }
                 }
-            } catch(NullPointerException | ConcurrentModificationException ignore){}              //if ac doesn't provide required data, don't include it | if other Thread is accessing aircraft List, it can't be used to update icons
+            } catch (NullPointerException | ConcurrentModificationException ignore) {
+            }              //if ac doesn't provide required data, don't include it | if other Thread is accessing aircraft List, it can't be used to update icons
         });
     }
 }
